64e59e138fa253e2e37dacd4760cf2276d376f7c
Jonathan Knowles <mail@jonathanknowles.net>
2023-03-24 03:06:19 +0000

    Fix erroneous redundant import warnings in `Data.Monoid.LCM`.
    Certain definitions must be imported in order for Haddock to be able to
    construct hyperlinks within documentation, but are not actually used in
    any function.
    
    Frustratingly, GHC (and other tools such as HLS) mark these imports as
    redundant, even though they're actually needed by Haddock.
    
    This commit adds references to just these definitions (and no others)
    to prevent them from being marked as redundant imports. This makes it
    possible to compile the module with `-Wall -Werror`.

--- src/Data/Monoid/LCM.hs
+++ src/Data/Monoid/LCM.hs
@@ -1,4 +1,6 @@
-{-# LANGUAGE Haskell2010, FlexibleInstances #-}
+{-# LANGUAGE Haskell2010 #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 -- | This module defines the 'LCMMonoid' subclass of the 'Monoid' class.
 --
@@ -18,17 +20,15 @@ import Prelude hiding (gcd, lcm, max)
 import qualified Prelude
 
 import Data.IntSet (IntSet)
+import Data.Maybe (isJust)
 import Data.Monoid (Dual (..), Product (..), Sum (..))
 import Data.Monoid.GCD (GCDMonoid (..))
+import Data.Semigroup.Cancellative (Reductive ((</>)))
+import Data.Semigroup.Commutative (Commutative)
 import Data.Set (Set)
 import Numeric.Natural (Natural)
 import qualified Data.IntSet as IntSet
 import qualified Data.Set as Set
-
--- These imports are marked as redundant, but are actually required by haddock:
-import Data.Maybe (isJust)
-import Data.Semigroup.Cancellative (Reductive ((</>)))
-import Data.Semigroup.Commutative (Commutative)
 
 -- | Class of Abelian monoids that allow the /least common multiple/ to be
 --   found for any two given values.
@@ -141,3 +141,22 @@ instance (LCMMonoid a, LCMMonoid b, LCMMonoid c, LCMMonoid d) =>
   where
     lcm (a0, a1, a2, a3) (b0, b1, b2, b3) =
         (lcm a0 b0, lcm a1 b1, lcm a2 b2, lcm a3 b3)
+
+--------------------------------------------------------------------------------
+-- Internal
+--------------------------------------------------------------------------------
+
+-- The following definitions must be imported in order for Haddock to be able
+-- to construct hyperlinks within documentation, but are not actually used in
+-- any function. We reference them here to stop GHC from marking them as
+-- redundant imports:
+--
+_nonRedundantImports :: ()
+_nonRedundantImports = ()
+  where
+    _isJust :: Maybe m -> Bool
+    _isJust = isJust
+    _reduce :: Reductive m => m -> m -> Maybe m
+    _reduce = (</>)
+    _commutative :: Commutative m => ()
+    _commutative = ()
